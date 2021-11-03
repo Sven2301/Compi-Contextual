086f6754cce57e8197b215c593e25110632d9aa9
Steven <svengra01@estudiantec.cr>
2021-11-03 16:17:30 -0600

    Rangevardecl implementado

--- Contextual-Compiladores/src/Triangle/ContextualAnalyzer/Checker.java
+++ Contextual-Compiladores/src/Triangle/ContextualAnalyzer/Checker.java
@@ -993,6 +993,7 @@ public final class Checker implements Visitor {
 
 
   @Override
+  //IMPLEMENTADO @MARCO
   public Object visitRepeatForRange(RepeatForRange ast, Object o) {
     TypeDenoter e1Type = (TypeDenoter) ast.E.visit(this, null);
     if (!e1Type.equals(StdEnvironment.integerType))
@@ -1053,6 +1054,7 @@ public final class Checker implements Visitor {
 
 
   @Override
+  // IMPLEMENTADO @STEVEN
   public Object visitRecursiveProcFuncsDeclaration(RecursiveProcFuncsDeclaration ast, Object o) {
     this.recursion = true;
     ast.PFD.visit(this,null);
@@ -1061,6 +1063,7 @@ public final class Checker implements Visitor {
 
 
   @Override
+  //IMPLEMENTADO @STEVEN
   public Object visitLocalDeclaration(LocalDeclaration ast, Object o) {
     idTable.openScope();
     idTable.pushPrivFlag(false);
@@ -1076,13 +1079,20 @@ public final class Checker implements Visitor {
 
 
   @Override
+  //IMPLEMENTADO @STEVEN
   public Object visitRangeVarDecl(RangeVarDecl ast, Object o) {
-    // TODO Auto-generated method stub
+    ConstDeclaration controlVarDecl = new ConstDeclaration(ast.I,
+        new IntegerExpression(new IntegerLiteral("0", ast.position), ast.position), ast.position);
+    idTable.openScope();
+    controlVarDecl.visit(this, null);
+    if (controlVarDecl.duplicated)
+      reporter.reportError("identifier \"%\" already declared", ast.I.spelling, ast.position);
     return null;
   }
 
 
   @Override
+  //IMPLENTADO @STEVEN
   public Object visitInVarDecl(InVarDecl ast, Object o) {
     ast.T = (TypeDenoter) ast.E.visit(this, null);
     idTable.enter(ast.I.spelling, ast);
